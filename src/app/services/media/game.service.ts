b239d1e8de3852335a578cb7cbdaf47f6606dc02
Lucas Rollin <lrollin@enssat.fr>
2020-10-23 17:31:30 +0200

    Add user metadata rating to Games

--- src/app/services/media/game.service.ts
+++ src/app/services/media/game.service.ts
@@ -1,3 +1,5 @@
+import { GameMetaResponseDto } from './../../shared/models/DtoResponse/games/game-meta.model';
+import { GameMeta } from '../../shared/models/DtoResponse/games/GameMeta.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -8,6 +10,7 @@ export class GameService {
 
   private urlGetPopularGames = 'http://127.0.0.1:4040/api/game';
   private urlGetGenreGames = 'http://127.0.0.1:4040/api/game/genres';
+  private urlUserMeta = 'http://127.0.0.1:4040/api/game/';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -17,5 +20,17 @@ export class GameService {
 
   getGenres(): Promise<any> {
     return this.httpClient.get<any>(this.urlGetGenreGames).toPromise();
+  }
+
+  getUserMeta(gameId: number): Promise<GameMetaResponseDto> {
+    return this.httpClient.get<GameMetaResponseDto>(this.urlUserMeta + gameId + '/meta').toPromise();
+  }
+
+  savePurchasedState(gameId: number, gameMeta: any): Promise<GameMetaResponseDto> {
+    return this.httpClient.patch<GameMetaResponseDto>(this.urlUserMeta + gameId + '/meta', gameMeta).toPromise();
+  }
+
+  saveRating(gameId: number, gameMeta: any): Promise<GameMetaResponseDto> {
+    return this.httpClient.patch<GameMetaResponseDto>(this.urlUserMeta + gameId + '/meta', gameMeta).toPromise();
   }
 }
