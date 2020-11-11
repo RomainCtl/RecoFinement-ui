e0782a1956d29ed27d22aa994d1b5e30eb960369
Lucas Rollin <lrollin@enssat.fr>
2020-11-11 17:38:36 +0100

    Fix user data model

--- src/app/shared/models/DtoResponse/user-data.model.ts
+++ src/app/shared/models/DtoResponse/user-data.model.ts
@@ -2,7 +2,11 @@ import { User } from 'src/app/shared/models/user.model';
 export class UserDataDtoResponse {
   status: boolean;
   message: string;
-  content: User;
+  content: {
+    uuid: string;
+    email: string;
+    username: string;
+  };
   number_of_elements: 0;
   page: 0;
   total_pages: 0;
